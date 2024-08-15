9ab32010b5e87b6c7b9f5726b8f2d9661871265e
hasanujjamanjibon <hasanujjamanjibon2@gmail.com>
2024-08-15 22:20:04 +0600

    server setup with mongodb

---
+++ index.js
@@ -0,0 +1,40 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+/* middleware */
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.USERPASS}@cluster0.19fl87e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
